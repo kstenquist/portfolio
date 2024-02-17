71fd31b9d2ec471a95a1800e6edef26d4f963621
kstenquist <kestenquist22@gmail.com>
2024-02-17 12:03:42 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,21 @@
+const red = 250;
+const green = 235;
+const blue = 215;
+
+const header = document.getElementById("header");
+console.log(header.innerText);
+
+function changeColor() {
+    const scrollFactor = 1 + window.scrollY / 200;
+
+    const r = red / scrollFactor;
+    const g = green / scrollFactor;
+    const b = blue / scrollFactor;
+
+    const updatedColor = `rgb(${r}, ${g}, ${b})`;
+
+    header.style.backgroundColor = updatedColor;
+}
+changeColor();
+
+window.addEventListener('scroll', changeColor);
