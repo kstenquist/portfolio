1579d05f477bcce69af5317016838b2c33e25943
kstenquist <kestenquist22@gmail.com>
2024-03-07 20:02:47 -0700

    remove unused code; comment-out javascript that changes header color when scrolling

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const blue = 76;
 const header = document.getElementById("header");
 console.log(header.innerText);
 
-function changeColor() {
-    const scrollFactor = 1 + window.scrollY / 200;
+// This code changes the header color as the page scrolls
+// function changeColor() {
+//     const scrollFactor = 1 + window.scrollY / 200;
 
-    const r = red / scrollFactor;
-    const g = green / scrollFactor;
-    const b = blue / scrollFactor;
+//     const r = red / scrollFactor;
+//     const g = green / scrollFactor;
+//     const b = blue / scrollFactor;
 
-    const updatedColor = `rgb(${r}, ${g}, ${b})`;
+//     const updatedColor = `rgb(${r}, ${g}, ${b})`;
 
-    header.style.backgroundColor = updatedColor;
-}
-changeColor();
+//     header.style.backgroundColor = updatedColor;
+// }
+// changeColor();
 
 window.addEventListener('scroll', changeColor);
